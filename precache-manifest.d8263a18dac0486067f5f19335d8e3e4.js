9a6ca62dc0edcd4280efd23a4fa37e0db6231471
farhandzakyarvianto <farhandzaky25@gmail.com>
2020-07-16 23:31:47 +0700

    Updates

---
+++ precache-manifest.d8263a18dac0486067f5f19335d8e3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9dbea0a66f916d25c50c4ce01b9eb4a",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "5a3fa894d237a93b1a72",
+    "url": "/pomodoro-clock/static/css/main.4c42cd53.chunk.css"
+  },
+  {
+    "revision": "85025e10ea5e2a2fb28f",
+    "url": "/pomodoro-clock/static/js/2.e458bd7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.e458bd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3fa894d237a93b1a72",
+    "url": "/pomodoro-clock/static/js/main.dfa9ceb5.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
